954bdf850f7ab313bf33102669488eab7ecf7e94
Dhevadharshini-m <dhevadharshini.m@rently.com>
2025-06-16 11:17:22 +0530

    f1 file simple commit

---
+++ f1.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+struct hello
+{
+    char a;
+    char b;
+    int n;
+};
+
+
+int main(int argc, char const *argv[])
+{
+    struct hello h1;
+    printf("Size of struct hello: %zu bytes\n", sizeof(h1));
+    struct hello* ptr = &h1;
+    ptr->a='a';
+    ptr->b='b';
+
+
+    char* p=&(ptr->a);
+
+    printf("%d\n", *(p+2));
+
+    printf("Size of pointer to struct hello: %zu bytes\n", sizeof(ptr));
+
+    return 0;
+}
+
+
+// void fibo(int n){
+//     int a=1;
+//     int b=1;
+//     int c;
+//     printf("Fibonacci series: ");
+//     printf("%d %d ", a, b);
+//     for(int i=0;i<n;i++){
+//         c=a+b;
+//         printf("%d ", c);
+//         a=b;
+//         b=c;
+//     }
+// }
+
+// void fun(int* x){
+//     *x = 100; 
+// }
+
+// int sum(int n){
+//     if(n<=0){
+//         return 0;
+//     }
+//     return n+sum(n-1);
+// }
+// int main(){
+//     int n=10;
+//     // fibo(n);
+//     printf("Sum of first %d natural numbers: %d\n", n, sum(n));
+//     printf("\n");
+//     double p=9.0;
+//     for(int i=0;i<10;i++){
+//         p=p*7.1;
+//         if(p<=16){
+//             continue;
+//         }
+//         printf("Value of p after %d iterations: %.lf\n", i+1, p);
+//     }
+//     printf("\n");
+//     printf("Final value of p: %.lf\n", p);
+//     printf("End of the program.\n");
+
+//     int arr[5];
+//     for(int i=0; i<5; i++){
+//         arr[i] = i+1; // Example initialization
+//     }
+//     arr[2]=908;
+//     for(int i=0; i<5; i++){
+//         printf("Array element %d: %d\n", i, arr[i]);
+//     }
+
+//     int val=90;
+//     fun(&val);
+//     printf("Value of val after fun: %d\n", val);
+
+//     int c=4;
+//     int s=2;
+//     c<<s;
+//     printf("%d\n",c);
+//     return 0;
+// }
